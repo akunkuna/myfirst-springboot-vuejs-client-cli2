c36e9e7a3f73e5c2e4db7007c8c8d51fb79d23dc
liyankun <aliyankuna@gmail.com>
2018-12-25 22:59:41 +0900

    load image and video by for and if

--- src/components/result.vue
+++ src/components/result.vue
@@ -14,36 +14,18 @@
                     <el-button type="warning" icon="el-icon-star-off" circle></el-button>
                     <el-button type="danger" icon="el-icon-delete" circle></el-button>
                 </div>
-                <!-- div class="player">
-                  <video-player  class="vjs-custom-skin"
-                                 ref="videoPlayer"
-                                 :options="playerOptions"
-                                 :playsinline="true"
-                                 @play="onPlayerPlay($event)"
-                                 @pause="onPlayerPause($event)"
-                                 @ended="onPlayerEnded($event)"
-                                 @loadeddata="onPlayerLoadeddata($event)"
-                                 @waiting="onPlayerWaiting($event)"
-                                 @playing="onPlayerPlaying($event)"
-                                 @timeupdate="onPlayerTimeupdate($event)"
-                                 @canplay="onPlayerCanplay($event)"
-                                 @canplaythrough="onPlayerCanplaythrough($event)"
-                                 @ready="playerReadied"
-                                 @statechanged="playerStateChanged($event)">
-                  </video-player>
-              </div -->
             </el-card>
         </el-col>
       <viewer>
-      <el-col :span="4" v-for="(o) in 10" :key="o" :offset="0">
+      <el-col :span="4" v-for="cell in gridData" :key="cell" :offset="0">
         <el-card :body-style="{ padding: '0px'}">
-          <div v-if="o % 2 == 0">
-                <img src="../assets/images/ham02.jpg" class="image">
+          <div v-if="cell.type == 'image'">
+                <img v-bind:src="cell.src" class="image">
           </div>
-          <div v-else>
+          <div v-else-if="cell.type == 'video'">
                 <video-player class="vjs-custom-skin"
                                ref="videoPlayer"
-                               :options="playerOptions"
+                               :options="cell.playerOptions"
                                :playsinline="true"
                                @play="onPlayerPlay($event)"
                                @pause="onPlayerPause($event)"
@@ -61,7 +43,6 @@
           <div style="padding: 7px;">
             <span>Hambuger</span>
             <div class="bottom clearfix">
-              <!-- time class="time">{{ currentDate }}</time -->
               <el-button type="text" class="button" @click="imgClick">操作Button</el-button>
             </div>
           </div>
@@ -75,39 +56,118 @@ export default {
   name: 'Currency',
   data () {
     return {
-      currentDate: new Date(),
-      // videojs options
-      playerOptions: {
-        height: '180',
-        autoplay: false,
-        muted: true,
-        language: 'en',
-        playbackRates: [0.7, 1.0, 1.5, 2.0],
-        sources: [{
-          type: 'video/mp4',
-          // mp4
-          src: 'http://vjs.zencdn.net/v/oceans.mp4'
-          // webm
-          // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
-        }],
-        poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg',
-        controlBar: {
-          timeDivider: false,
-          durationDisplay: false,
-          remainingTimeDisplay: false,
-          volumePanel: false,
-          // playToggle: false,
-          // progressControl: false,
-          remainingTimeDisplay: false,
-          playbackRateMenuButton: false,
-          fullscreenToggle: true
-        }
-      }
+      gridData: [
+        {type: 'image', src: require('../assets/images/ham02.jpg')},
+        {type: 'image', src: require('../assets/images/ham01.png')},
+        {
+          type: 'video',
+          playerOptions: {
+            height: '180',
+            autoplay: false,
+            muted: true,
+            language: 'en',
+            playbackRates: [0.7, 1.0, 1.5, 2.0],
+            sources: [{
+              type: 'video/mp4',
+              src: 'http://vjs.zencdn.net/v/oceans.mp4'
+            }],
+            poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg',
+            controlBar: {
+              timeDivider: false,
+              durationDisplay: false,
+              remainingTimeDisplay: false,
+              volumePanel: false,
+              playbackRateMenuButton: false,
+              fullscreenToggle: true
+            }
+          }
+        },
+        {
+          type: 'video',
+          playerOptions: {
+            height: '180',
+            autoplay: true,
+            muted: true,
+            language: 'en',
+            playbackRates: [0.7, 1.0, 1.5, 2.0],
+            sources: [{
+              type: 'video/mp4',
+              src: 'http://vjs.zencdn.net/v/oceans.mp4'
+            }],
+            poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg',
+            controlBar: {
+              timeDivider: false,
+              durationDisplay: false,
+              remainingTimeDisplay: false,
+              volumePanel: false,
+              playbackRateMenuButton: false,
+              fullscreenToggle: true
+            }
+          }
+        },
+        {type: 'image', src: require('../assets/images/ham02.jpg')},
+        {type: 'image', src: require('../assets/images/ham01.png')},
+        {type: 'image', src: require('../assets/images/ham02.jpg')},
+        {type: 'image', src: require('../assets/images/ham01.png')},
+        {
+          type: 'video',
+          playerOptions: {
+            height: '180',
+            autoplay: false,
+            muted: true,
+            language: 'en',
+            playbackRates: [0.7, 1.0, 1.5, 2.0],
+            sources: [{
+              type: 'video/mp4',
+              src: 'http://vjs.zencdn.net/v/oceans.mp4'
+            }],
+            poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg',
+            controlBar: {
+              timeDivider: false,
+              durationDisplay: false,
+              remainingTimeDisplay: false,
+              volumePanel: false,
+              playbackRateMenuButton: false,
+              fullscreenToggle: true
+            }
+          }
+        },
+        {
+          type: 'video',
+          playerOptions: {
+            height: '180',
+            autoplay: true,
+            muted: true,
+            language: 'en',
+            playbackRates: [0.7, 1.0, 1.5, 2.0],
+            sources: [{
+              type: 'video/mp4',
+              src: 'http://vjs.zencdn.net/v/oceans.mp4'
+            }],
+            poster: 'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg',
+            controlBar: {
+              timeDivider: false,
+              durationDisplay: false,
+              remainingTimeDisplay: false,
+              volumePanel: false,
+              playbackRateMenuButton: false,
+              fullscreenToggle: true
+            }
+          }
+        },
+        {type: 'image', src: require('../assets/images/ham02.jpg')},
+        {type: 'image', src: require('../assets/images/ham01.png')}
+      ]
     }
   },
   methods: {
     buttonClick: function () {
       this.$router.push({ path: 'search' })
+    },
+    onPlayerLoadeddata: function () {
+      // this.$router.push({ path: 'search' })
+      console.log('video loaded')
+      console.log(this.playerOptions.sources[0])
     }
   }
 }
@@ -119,6 +179,7 @@ export default {
 .image {
   width: 100%;
   display: block;
+  height: 180px;
 }
 
 </style>
